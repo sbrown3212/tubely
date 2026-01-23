1c17a1305a489fe3803a6672b3123a9ff1c005af
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-01-23 11:43:26 -0700

    Implement JWT validation in handlerUploadVideo.

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,7 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Invalid JWT", err)
+		return
+	}
+
+	fmt.Println("Uploading video", videoID, "by user", userID)
+}
