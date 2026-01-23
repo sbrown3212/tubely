ef728bc89cdad5199bc19b0e801d782c296b20db
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-01-23 12:45:32 -0700

    Remove print statement from handlerUploadThumbnail.

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -31,8 +31,6 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
-
 	const maxMemory = 10 << 20
 	err = r.ParseMultipartForm(int64(maxMemory))
 	if err != nil {
