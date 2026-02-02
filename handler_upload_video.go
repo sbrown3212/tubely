cee32befbfbc1a09d57ca6e81a3b3e40fbc012a8
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-02-02 14:53:19 -0700

    Update video url to use CF distribution in video upload handler.

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -154,7 +154,7 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	url := fmt.Sprintf("%s,%s", cfg.s3Bucket, key)
+	url := fmt.Sprintf("%s/%s", cfg.s3CfDistribution, key)
 	dbVideo.VideoURL = &url
 
 	err = cfg.db.UpdateVideo(dbVideo)
